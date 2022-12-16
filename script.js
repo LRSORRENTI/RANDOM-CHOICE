c300d7dd6086278a10be45313dfc7ed38a2de865
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-12-15 20:25:36 -0600

    added functions to randomly select one of the inputs the user submits

--- script.js
+++ script.js
@@ -34,7 +34,12 @@ textarea.focus()
 textarea.addEventListener('keyup', (e) => {
     createtags(e.target.value)
     //this is listening for the keyup event
-
+if(e.key === 'Enter'){
+    setTimeout(() => {
+e.target.value = ''
+    }, 10)
+    randomSelect()
+}
 })
 
 function createtags(input){
@@ -50,3 +55,36 @@ function createtags(input){
         tagsEl.appendChild(tagEl)
     })
 }
+
+function randomSelect(){
+    const times = 30
+    const times100 = times * 100;
+    const interval = setInterval(() =>{
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(() =>{
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100) 
+
+    setTimeout(() =>{
+            clearInterval(interval)
+            setTimeout(() => {
+
+            }, 100)
+    }, times100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+function highlightTag(tag){
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag){
+    tag.classList.remove('highlight')
+}
+
